58450787b7ade1303a005baa7333fdc20d09196d
Joakim Boström <joakim.bostrom43@gmail.com>
2023-11-04 19:56:38 +0100

    S23-26 gimbal camera added (#5)

--- assets/scripts/PlayerCamera.cs
+++ assets/scripts/PlayerCamera.cs
@@ -1,23 +1,36 @@
 using Godot;
 using System;
 
-public partial class PlayerCamera : Camera3D
+public partial class PlayerCamera : Node3D
 {
 	public Node3D target;
-	public Node3D position;
+
 	[Export] private float _lerpSpeed = 10.0f;
 	[Export] private float _distanceToTarget = 5.0f;
 	[Export] private float _heightToTarget = 5.0f;
 	[Export] private float _angleOffset = 10.0f;
+	[Export] private float _RotationSpeed = 0.005f;
+	[Export] private Node3D _outerGimbal;
+	[Export] private Node3D _innerGimbal;
+
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		if (target == null || position == null) 
+		if (target == null) 
 		{
 			return;
 		}
-		GlobalTransform = GlobalTransform.InterpolateWith(position.GlobalTransform, _lerpSpeed * (float) delta);
-		LookAt(target.Position);
+		
+		GlobalTransform = GlobalTransform.InterpolateWith(target.GlobalTransform, _lerpSpeed * (float) delta);
 	}
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion) {
+			var mouseEvent = (InputEventMouseMotion) @event;
+			_outerGimbal.RotateObjectLocal(Vector3.Up, -mouseEvent.Relative.X * _RotationSpeed);
+			_innerGimbal.RotateObjectLocal(Vector3.Right, -mouseEvent.Relative.Y * _RotationSpeed);
+		}
+    }
 }
