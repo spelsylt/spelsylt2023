cba428a4dd66f732b0afe270382d0c2b1208dea2
= <joakim.bostrom43@gmail.com>
2023-11-04 21:29:17 +0100

    fixed better brakes

--- assets/scripts/PhysicsCar.cs
+++ assets/scripts/PhysicsCar.cs
@@ -9,14 +9,18 @@ public partial class PhysicsCar : VehicleBody3D
 	[Export] private float _maxTorque = 50;
 	[Export] private float _steeringSpeed = 5.0f;
 	[Export] private PlayerCamera _playerCamera;
+	[Export] private float _brakeForce = 50.0f;
+	[Export] private float _handbrakeForce = 100.0f;
 
 	private List<VehicleWheel3D> _drivingWheels;
+	private List<VehicleWheel3D> _handbrakeWheels;
 
     public override void _Ready()
     {
         base._Ready();
 		_playerCamera.target = this;
 		_drivingWheels = GetDrivingWheels();
+		_handbrakeWheels = GetHandbrakeWheels();
     }
 
     public override void _PhysicsProcess(double delta)
@@ -24,11 +28,23 @@ public partial class PhysicsCar : VehicleBody3D
         Steering = Mathf.Lerp(Steering, Input.GetAxis("steer_right", "steer_left") * 0.4f, _steeringSpeed * (float)delta);
 		var acceleration = Input.GetAxis("brake_reverse", "accelerate") * _acceleration;
 		_drivingWheels.ForEach(x => ApplyAcceleration(x, acceleration));
+
+		if (Input.IsActionPressed("handbrake")) {
+			_handbrakeWheels.ForEach(wheel => wheel.Brake = _handbrakeForce);
+		} else {
+			_handbrakeWheels.ForEach(wheel => wheel.Brake = 0.0f);
+		}
     }
 
 	private void ApplyAcceleration(VehicleWheel3D wheel, float acceleration) {
 		var rpm = Mathf.Abs(wheel.GetRpm());
-		wheel.EngineForce = acceleration * _maxTorque * (1 - rpm / _maxRPM);
+		if ((acceleration < 0 && wheel.GetRpm() > 0) || acceleration > 0 && wheel.GetRpm() < 0) {
+			wheel.Brake = _brakeForce;
+			wheel.EngineForce = 0f;
+		} else {
+			wheel.Brake = 0f;
+			wheel.EngineForce = acceleration * _maxTorque * (1 - rpm / _maxRPM);
+		}
 	}
 
 	protected List<VehicleWheel3D> GetDrivingWheels() {
@@ -36,6 +52,14 @@ public partial class PhysicsCar : VehicleBody3D
         {
             GetNode<VehicleWheel3D>("BackLeftWheel"),
             GetNode<VehicleWheel3D>("BackRightWheel")
+        };
+	}
+
+	protected List<VehicleWheel3D> GetHandbrakeWheels() {
+		return new List<VehicleWheel3D>
+        {
+            GetNode<VehicleWheel3D>("FrontLeftWheel"),
+            GetNode<VehicleWheel3D>("FrontRightWheel")
         };
 	}
 }
