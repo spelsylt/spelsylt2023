4e8958fca631e9ab5475aa4c7c6c4a5752513f3c
simpan22 <simpan22@gmail.com>
2023-11-11 17:21:55 +0100

    Rotate camera to behind car after 1s without mouse input (#12)

--- assets/scripts/PlayerCamera.cs
+++ assets/scripts/PlayerCamera.cs
@@ -3,8 +3,10 @@ using System;
 
 public partial class PlayerCamera : Node3D
 {
-	public Node3D target;
+	[Export] public VehicleBody3D _car;
 
+    [Export] private float _mouseCaptureDelay = 1.0f;
+    [Export] private float _cameraStiffness = 1.0f;
 	[Export] private float _lerpSpeed = 10.0f;
 	[Export] private float _distanceToTarget = 5.0f;
 	[Export] private float _heightToTarget = 5.0f;
@@ -13,16 +15,30 @@ public partial class PlayerCamera : Node3D
 	[Export] private Node3D _outerGimbal;
 	[Export] private Node3D _innerGimbal;
 
+    private double _mouseControleCountdown = 0.0f;
+
+    public override void _Ready() {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		if (target == null) 
+		if (_car == null) 
 		{
 			return;
 		}
 		
-		GlobalTransform = GlobalTransform.InterpolateWith(target.GlobalTransform, _lerpSpeed * (float) delta);
+		GlobalTransform = GlobalTransform.InterpolateWith(_car.GlobalTransform, _lerpSpeed * (float) delta);
+        if (_mouseControleCountdown <= 0.0f) {
+            var newYaw = Mathf.LerpAngle(_outerGimbal.Rotation.Y, MathF.PI, (float)delta / _cameraStiffness);
+            var newPitch = Mathf.LerpAngle(_innerGimbal.Rotation.X, -MathF.PI / 8, (float)delta / _cameraStiffness);
+
+            _outerGimbal.Rotation = new Vector3(0.0f, newYaw, 0.0f);
+            _innerGimbal.Rotation = new Vector3(newPitch, 0.0f, 0.0f);
+        } else {
+            _mouseControleCountdown -= delta;
+        }
 	}
 
     public override void _Input(InputEvent @event)
@@ -30,6 +46,7 @@ public partial class PlayerCamera : Node3D
         if (@event is InputEventMouseMotion mouseEvent) {
 			_outerGimbal.RotateObjectLocal(Vector3.Up, -mouseEvent.Relative.X * _RotationSpeed);
 			_innerGimbal.RotateObjectLocal(Vector3.Right, -mouseEvent.Relative.Y * _RotationSpeed);
-		}
+            _mouseControleCountdown = _mouseCaptureDelay;
+        }
     }
 }
